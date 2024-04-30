77875dcfed6ff04ff20103140a8c06b4401830c9
Boonsong Srithong <song@odds.team>
2024-04-30 08:36:57 +0700

    build keycloak image

--- login_themes/src/keycloak-theme/mobile/kcContext.ts
+++
@@ -1,7 +0,0 @@
-import { getKcContext } from "keycloakify/login";
-
-export type KcContext = NonNullable<
-  ReturnType<typeof getKcContext>["kcContext"]
->;
-
-export const { kcContext } = getKcContext({});
